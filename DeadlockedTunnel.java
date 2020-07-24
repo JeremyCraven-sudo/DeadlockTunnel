bc15fe5ccc3a24d5d8381c8932752fbe5db6f364
Cody Phillips <cody2533@gmail.com>
2020-07-23 20:17:47 -0400

    Update DeadlockedTunnel.java
    Added infinte loop and a few sleep timiers to slow it so it is readable.

--- DeadlockedTunnel.java
+++ DeadlockedTunnel.java
@@ -40,18 +40,24 @@ class Util {
 class Tunnel {
 	// odd car number means right-bound while even car number is left-bound
 	synchronized void tunnelTime(int carNumber) {// synchronized method
-		if (carNumber % 2 == 0) {
-			LeftBoundCars lbc = new LeftBoundCars();
-			System.out.println("Left-bound Car " + lbc.getCarNumber() + " is in the tunnel.");
-			lbc.setCarNumber(lbc.getCarNumber() + 2);
-		} else {
-			RightBoundCars rbc = new RightBoundCars();
-			System.out.println("Right-bound Car " + rbc.getCarNumber() + " is in the tunnel.");
-			rbc.setCarNumber(rbc.getCarNumber() + 2);
-		}
 		try {
 			Random r = new Random();
 			Util.sleep(r.nextInt(500));
+		} catch (Exception e) {
+			System.out.println(e);
+		}
+		if (carNumber % 2 == 0) {
+			
+			System.out.println("Left-bound Car " + carNumber + " is in the tunnel.");
+			
+		} else {
+			
+			System.out.println("Right-bound Car " + carNumber + " is in the tunnel.");
+			
+		}
+		try {
+			Random r = new Random();
+			Util.sleep(r.nextInt(1500));
 		} catch (Exception e) {
 			System.out.println(e);
 		}
@@ -69,6 +75,7 @@ public class DeadlockedTunnel {
 		LeftCarThread leftCar = new LeftCarThread();
 		RightCarThread rightCar = new RightCarThread();
 		
+		
 		leftCar.start();
 		rightCar.start();
 		
@@ -77,18 +84,51 @@ public class DeadlockedTunnel {
 	private static class LeftCarThread extends Thread {
 		public void run() {
 			LeftBoundCars lbc = new LeftBoundCars();
-			System.out.println("Left-bound Car " + lbc.getCarNumber() + " wants to enter the tunnel.");
-			tunnel.tunnelTime(lbc.getCarNumber());
-			System.out.println("Left-bound Car " + lbc.getCarNumber() + " has left the tunnel.");
+			
+			while(true) {
+				System.out.println("Left-bound Car " + lbc.getCarNumber() + " wants to enter the tunnel.");
+				try {
+					Random r = new Random();
+					Util.sleep(r.nextInt(1500));
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				tunnel.tunnelTime(lbc.getCarNumber());
+				System.out.println("Left-bound Car " + lbc.getCarNumber() + " has left the tunnel.");
+				lbc.setCarNumber(lbc.getCarNumber() + 2);
+				try {
+					Random r = new Random();
+					Util.sleep(r.nextInt(1500));
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
+			
 		}
 	}
 
 	private static class RightCarThread extends Thread {
 		public void run() {
 			RightBoundCars rbc = new RightBoundCars();
-			System.out.println("Right-bound Car " + rbc.getCarNumber() + " wants to enter the tunnel.");
-			tunnel.tunnelTime(rbc.getCarNumber());
-			System.out.println("Right-bound Car " + rbc.getCarNumber() + " has left the tunnel.");
+			
+			while(true) {
+				System.out.println("Right-bound Car " + rbc.getCarNumber() + " wants to enter the tunnel.");
+				try {
+					Random r = new Random();
+					Util.sleep(r.nextInt(1500));
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+				tunnel.tunnelTime(rbc.getCarNumber());
+				System.out.println("Right-bound Car " + rbc.getCarNumber() + " has left the tunnel.");
+				rbc.setCarNumber(rbc.getCarNumber() + 2);
+				try {
+					Random r = new Random();
+					Util.sleep(r.nextInt(1500));
+				} catch (Exception e) {
+					System.out.println(e);
+				}
+			}
 		}
 	}
 }
